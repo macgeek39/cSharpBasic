2f374a365f331d8b9fb913fcccd236c8e91e6d4c
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2023-12-28 05:50:54 -0600

    Initialized Commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+// Welcome User to App
+Console.WriteLine("Welcome to the Greeting Application");
+Console.WriteLine("This application is buit by Mike");
+Console.WriteLine("-------------------------------------");
+Console.WriteLine();
+
+// Ask for first Name
+Console.Write("Enter your first name: ");
+string firstName;
+firstName = Console.ReadLine();
+
+
+// Greet user by name
+Console.WriteLine();
+Console.WriteLine("Hello " + firstName);
